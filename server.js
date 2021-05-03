fa60c3084d7575a762252a449faf2854d18ee163
MKAbuMattar <mohammad.khaled@outlook.com>
2021-05-03 22:43:53 +0300

    City Explorer/Express Server/Data Weather

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const data = require('./assets/data/weather.json')
+const PORT = process.env.PORT
+const app = express()
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send(data)
+})
+
+app.listen(PORT)
