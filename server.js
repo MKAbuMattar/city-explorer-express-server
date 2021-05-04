acdec7f3195852b32f2d17a4a7c628d8233c2920
MKAbuMattar <mohammad.khaled@outlook.com>
2021-05-04 21:20:25 +0300

    hotfix

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const data = require('./assets/data/weather.json');
+const weather = require('./assets/data/weather.json');
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-  res.send(data);
+
+
+
+  res.send('<blockquote>Sometimes success comes in ways you don\'t expect.<br><cite>&mdash;by BEN BARNES</cite></blockquote>');
 });
+
+app.get('/weather', function (req, res) {
+  const formatData = weather.data.map(result => new Weather(result));
+  res.send(formatData);
+});
+
+app.get('/movie', function (req, res) {
+  res.send('movie');
+});
+
+class Weather {
+  constructor(data) {
+    this.date = data.valid_date;
+    this.temp = data.temp;
+    this.windCdir = data.wind_cdir;
+    this.minTemp = data.min_temp;
+    this.maxTemp = data.max_temp;
+    this.description = data.weather.description;
+  }
+}
+
 
 app.listen(PORT);
